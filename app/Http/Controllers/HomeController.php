73cb50ab7778f22f7c30087acc31766df0089c9e
Tomas Glezl <tmsglezl@gmail.com>
2021-07-12 11:28:24 +0200

    basic commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
     public function index()
     {
         $countries = Country::all();
-        
 
         return view('home', compact('countries'));
     }
