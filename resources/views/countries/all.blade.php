8b3a88080a502879e322835a970435d982366398
Tomas Glezl <tmsglezl@gmail.com>
2021-07-16 13:14:09 +0200

    super last styling

--- resources/views/countries/all.blade.php
+++ resources/views/countries/all.blade.php
@@ -19,26 +19,20 @@
 
     <div class="country_all country__card">
         <div class="country__card"> 
-            <a  class="teas__all__ul__li__a" href='/countries/1'>China</a>
-        </div>
-        <div class="country__card"> 
-            <a  class="teas__all__ul__li__a" href='/countries/2'>India</a>
-        </div>
-        <div>
-            <div class="country__card"> 
-                <a  class="teas__all__ul__li__a" href='/region/2'>Asia</a> 
-            </div> 
-            <div class="country__card"> 
-                <a  class="teas__all__ul__li__a" href='/region/3'>Africa</a>
-            </div>
-        </div>
-        <div>
-            <div class="country__card"> 
-                <a  class="teas__all__ul__li__a" href='/region/1'>South America</a>
-            </div>
-            <div class=" country__card"> 
-                <a  class="teas__all__ul__li__a" href='/region/4'>Oceania</a>
-            </div>
+            <ul>
+              <li>  <a  class="teas__all__ul__li__a" href='/countries/1'>China</a></li>
+            
+              <li>  <a  class="teas__all__ul__li__a" href='/countries/2'>India</a></li>
+            
+                <li> <a  class="teas__all__ul__li__a" href='/region/2'>Asia</a> </li>
+            
+                    <li> <a  class="teas__all__ul__li__a" href='/region/3'>Africa</a></li>
+                
+        
+                        <li>  <a  class="teas__all__ul__li__a" href='/region/1'>South America</a></li>
+                
+                            <li> <a  class="teas__all__ul__li__a" href='/region/4'>Oceania</a></li>
+            </ul>
         </div>
     </div>
 
