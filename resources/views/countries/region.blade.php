a94f7f0055ae035114486ed6c4744d6aa0c4e308
petrKavulok <petr.kavulok@gmail.com>
2021-07-16 13:35:17 +0200

    another final update

--- resources/views/countries/region.blade.php
+++ resources/views/countries/region.blade.php
@@ -1,41 +1,35 @@
 @extends('layouts.main')
 
 @section('content')
-<div className="route">
+<div class="route">
     <a href='/'>Home</a> -> <a href='/countries'>Countries</a>->{{$region[0]->name}}
+</div>
+
+        <h1>{{$region[0]->name}}</h1>
 <div>
     <div class="teas__all basic_styling_for_links ">
-<h1>{{$region[0]->name}}</h1>
 
+        @if (sizeof($countries) > 0)
+            <ul>
+                @foreach($countries as $country)
+                    <li>
+                        <a href={{ action('CountryController@show', $country->id) }}>{{$country->name}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <br>
+                No countries from {{$region[0]->name}} in our database yet!
+        @endif
+    </div>        
 
+    <div class='map main__home__section main__home__section__map' style="width: 1136px; height:500px; background-color: rgba(113, 128, 185, 0.8);"></div>
 
-@if (sizeof($countries) > 0)
-<ul>
-@foreach($countries as $country)
+    <script>
+        window.mapData = '{!! json_encode([$countries, $region]) !!}';
+    </script>
 
-<li>
-<a href={{ action('CountryController@show', $country->id) }}>{{$country->name}}</a>
-
-
-</li>
-
-@endforeach
-</ul>
-@else
-<br>
-No countries from {{$region[0]->name}} in our database yet!
-@endif
-
-
-<div class='map' style="width: 1136px; height:500px"></div>
-
-
-<script>
-
-window.mapData = '{!! json_encode([$countries, $region]) !!}';
-
-
-</script>
-<script src="{{mix('js/continent.js')}}"></script>
+        <script src="{{mix('js/continent.js')}}"></script>
+ 
 </div>
 @endsection
