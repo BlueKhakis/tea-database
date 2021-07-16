a6b68ece858d9d3ede4e07fcef2531926db593af
petrKavulok <petr.kavulok@gmail.com>
2021-07-16 13:15:53 +0200

    last final update

--- resources/views/basics.blade.php
+++ resources/views/basics.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.main')
-
 @section('content')
-<a href='/'>Home</a> -> Basics
+
+<div class="route">
+    <a href='/'>Home</a> -> Basics
+</div>
+
 <div class="teas__all basic_styling_for_links ">
 <h1>Basics</h1>
 <h2>Get started</h2>
 <div class="loremipsum">
    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At, perspiciatis. Unde, et itaque? Accusantium iure saepe officiis autem, deleniti voluptatibus velit maxime magni delectus suscipit harum eum aliquam culpa laboriosam!<br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla laborum quas vel fugit nesciunt non cumque? Harum dolorum cum eaque nulla officiis id saepe tempora ad adipisci! Beatae, repellendus ducimus?
+    Esse quis repudiandae molestias quo quisquam dolores error cum vel culpa! Dolorum sed qui molestiae voluptates cumque soluta quis quam assumenda nulla perferendis beatae, delectus non sapiente dolor ipsam placeat?
+    Ex distinctio quaerat sapiente, debitis animi saepe exercitationem odio iste possimus temporibus delectus veniam in facere numquam quidem, id velit, earum cumque omnis doloremque nam adipisci commodi quam suscipit. Placeat?
+    Enim aspernatur pariatur incidunt distinctio nostrum ab optio fugiat mollitia quisquam vitae velit vel ipsum excepturi corporis et possimus ducimus eum, temporibus nemo dolorum quia eos ut consectetur omnis. Placeat.</p>
 </p>
 </div>
     <br>
@@ -23,4 +30,5 @@ Top 6 teas
 @endforeach
 </ul>
 </div>
+
 @endsection
