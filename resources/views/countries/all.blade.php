cc23edd3c4b34e3515c3224d68bb2db91c171a3c
petrKavulok <petr.kavulok@gmail.com>
2021-07-16 08:53:58 +0200

    yo

--- resources/views/countries/all.blade.php
+++ resources/views/countries/all.blade.php
@@ -1,25 +1,46 @@
 @extends('layouts.main')
 @section('content')
+
 <div class="route">
     <a href='/'>Home</a> -> Countries
-
 </div>    
-<div class="basic_styling_for_links user__page">    
 
-    <ul class="teas__all__ul">
-        <li class="teas__all__ul__li"> 
+ <script>
+            window.mapData = '{!! json_encode($countries) !!}';
+        </script>
+
+        <div style="background-color: rgba(113, 128, 185, 1);" class="main__home__section main__home__section__map"> 
+        </div>
+
+<script src="{{mix('js/map.js')}}">
+        </script>
+
+<div class="basic_styling user__page">    
+
+    <div class="country_all country__card">
+        <div class="country__card"> 
             <a  class="teas__all__ul__li__a" href='/countries/1'>China</a>
-        </li>
-        <li class="teas__all__ul__li"> 
+        </div>
+        <div class="country__card"> 
             <a  class="teas__all__ul__li__a" href='/countries/2'>India</a>
-        </li>
-        <li class="teas__all__ul__li"> 
-            <a  class="teas__all__ul__li__a" href='/region/2'>Asia</a> <a  class="teas__all__ul__li__a" href='/region/3'>Africa</a>
-        </li>
-        <li class="teas__all__ul__li"> 
-            <a  class="teas__all__ul__li__a" href='/region/1'>South America</a> <a  class="teas__all__ul__li__a" href='/region/4'>Oceania</a>
-        </li>
-    </ul>
+        </div>
+        <div>
+            <div class="country__card"> 
+                <a  class="teas__all__ul__li__a" href='/region/2'>Asia</a> 
+            </div> 
+            <div class="country__card"> 
+                <a  class="teas__all__ul__li__a" href='/region/3'>Africa</a>
+            </div>
+        </div>
+        <div>
+            <div class="country__card"> 
+                <a  class="teas__all__ul__li__a" href='/region/1'>South America</a>
+            </div>
+            <div class=" country__card"> 
+                <a  class="teas__all__ul__li__a" href='/region/4'>Oceania</a>
+            </div>
+        </div>
+    </div>
 
 </div>
 
